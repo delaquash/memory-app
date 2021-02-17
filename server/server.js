905a54363a067f52c2d01791ecd70786e3646fcc
delaquash <olaidemmauel1@gmail.com>
2021-02-17 09:39:46 +0100

    fixed user login and sign up

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error')
 const app = express();
 app.use(bodyParser.json())
 
 app.use('/api/places',placesRoutes);
+app.use('/api/users', usersRoutes);
 
 // Handling errors for unsupported routes
 app.use((req, res, next) => {
