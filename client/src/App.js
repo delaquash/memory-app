99defddf5e4c344f7b1534ee97a437010a3c3c2f
delaquash <olaidemmauel1@gmail.com>
2021-03-23 14:14:48 +0100

    installed react-transition-group to fix animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import User from './user/pages/Users';
 import NewPlaces from './places/pages/NewPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+import './App.css';
 
 const App =() =>{
   return (
