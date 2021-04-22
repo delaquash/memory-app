e7b2f4c9a5f273a79811a3fd3e7175e36f9dca31
delaquash <olaidemmauel1@gmail.com>
2021-04-22 06:15:42 +0100

    fixed modal and spinner for loading

---
+++ client/src/shared/components/UIElements/ErrorModal.js
@@ -0,0 +1,20 @@
+
+
+import Button from '../FormElements/Button';
+import Modal from './Modal';
+
+
+const ErrorModal = props=> {
+    return(
+        <Modal 
+            onCancel={props.clear}
+            header="An error occurred"
+            show={!!props.error}
+            footer={<Button onClick={props.onClear}></Button>}
+        >
+            <p>{props.error}</p>
+        </Modal>
+    )
+}
+
+export default ErrorModal;
