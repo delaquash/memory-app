5e9ad2af03c17bdea9df26c4761d07989a2da242
delaquash <olaidemmauel1@gmail.com>
2021-04-22 05:34:34 +0100

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -23,6 +23,17 @@ app.use((req, res, next) => {
     throw error;
 })
 
+// handling cors
+app.use((req, res, next) => {
+    res.setHeader('Access=Control-Allow-Origin', 'x');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next()
+})
+
 
 // Error handling middleware
 app.use((error, req, res, next) => {
