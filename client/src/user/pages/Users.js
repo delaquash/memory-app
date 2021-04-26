9cb5dd56e0f48ca22728dc0a53a808e46b0c80d8
delaquash <olaidemmauel1@gmail.com>
2021-04-26 06:40:53 +0100

    fixed useEffect hooks to get user request

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,16 +1,12 @@
+import { useEffect } from 'react';
 import UserList from '../components/UserList';
 const Users =() =>{
-    const USERS = [{
-        id: 'ul',
-        name: 'Max Schwars',
-        image: "https://images.unsplash.com/photo-1485856407642-7f9ba0268b51?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8amF2YXNjcmlwdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
-        places: 3
-    }]
-    return (
-        <div>
-            <UserList items={USERS}/>
-        </div>
-    )
+   useEffect(() => {
+       return (
+        <UserList items={USERS}/>
+       ) 
+   }, [])
+
 }
 
 export default Users;
